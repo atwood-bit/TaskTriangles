5c1971753e139a5b641e2f670d6913f0ad6d85d8
Artsem Zinko <artsem.zinko-c@therapybrands.com>
2023-07-07 20:25:22 +0300

    Add project files.

---
+++ TaskTriangles/Extensions/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskTriangles.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color ChangeColorBrightness(this Color color, double correctionFactor)
+        {
+            double red = color.R;
+            double green = color.G;
+            double blue = color.B;
+
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+
+            return Color.FromArgb(color.A, (int)red, (int)green, (int)blue);
+        }
+    }
+}
---
+++ TaskTriangles/Extensions/FigureExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTriangles.Extensions
+{
+    public static class FigureExtensions
+    {
+        public static void FillTriangle(this Graphics graphic, Point[] points, Color color, double transparency)
+        {
+            var brush = new SolidBrush(color.ChangeColorBrightness(transparency));
+            graphic.FillPolygon(brush, points);
+        }
+    }
+}
